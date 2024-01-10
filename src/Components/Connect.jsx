660b122d22e5d086b695eff50c88a87ce217d7f4
Olusola Odunwo-Albert <shola.albert@gmail.com>
2024-01-10 22:08:13 +0100

    corrected socials

--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -88,11 +88,11 @@ const Connect = () => {
           follow the journey towards breaking records for a cause.
         </p>
         <div className=" flex items-center gap-2 lg:gap-5 py-10">
-          <a href="https://www.instagram.com/pushupmania_?igsh=NTc4MTIwNjQ2YQ==" target="_blank">
+          <a href="https://twitter.com/pushupmania_?t=yrPTdaoQuoomOVeXJb6XAA&s=09" target="_blank">
              <img src={Insta}  className=" w-[50px] lg:w-[80px]" />
           </a>
 
-        <a href="https://twitter.com/pushupmania_?t=yrPTdaoQuoomOVeXJb6XAA&s=09" target="_blank">
+        <a href="https://www.instagram.com/pushupmania_?igsh=NTc4MTIwNjQ2YQ==" target="_blank">
              <img src={Tw}  className=" w-[50px] lg:w-[80px]" />
           </a>
 
